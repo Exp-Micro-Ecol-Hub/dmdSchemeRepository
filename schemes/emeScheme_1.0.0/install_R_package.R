fc3c521d36fa86962a1dafaca9d61c38e2b6d06c
Rainer M. Krug <Rainer@krugs.de>
2021-01-20 10:38:55 +0100

    Add emeScheme v1.0

---
+++ schemes/emeScheme_1.0.0/install_R_package.R
@@ -0,0 +1,11 @@
+rold <- getOption("repos")
+r <- rold
+
+r["CRAN"] <-  "http://cran.us.r-project.org"
+r["dmdScheme"] <- "https://exp-micro-ecol-hub.github.io/dmdSchemeRMaster/"
+
+options(repos = r)
+
+install.packages("emeScheme")
+
+options(repos = rold)
